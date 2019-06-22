d6a7b42e5a808143aa5fb9fb2cbedd474d495c17
DESKTOP-D2NIS90\rwj84 <847679857@qq.com>
2019-06-22 22:41:46 +0800

    新增少换乘方案

--- src/views/cdsub.vue
+++ src/views/cdsub.vue
@@ -23,17 +23,36 @@
                     <form-item label="计算最短路线">
                         <Button type="primary" @click="planning" :disabled="isplan">计算路程</Button>
                     </form-item>
-                    <form-item label="最短路线"/>
-                    <div class="step-map">
-                        <div v-for="(item, index) in line" :key="index">
-                            <i v-if="index === 0">起</i>
-                            <i v-if="index === line.length - 1">始</i>
-                            <i v-if="item.isChange">转</i>
-                            <div :style="lineStyle(item.changeLine)">
-                                <strong>{{index+1}}</strong>
-                            </div>
-                            <span>{{item.name}}</span>
-                            <p>{{item.des}}</p>
+                    <div class="map-show">
+                        <div>
+                            <form-item label="最短路线"/>
+                            <div class="step-map">
+                                <div v-for="(item, index) in mastMinStation" :key="index">
+                                    <i v-if="index === 0">起</i>
+                                    <i v-if="index === mastMinStation.length - 1">始</i>
+                                    <i v-if="item.isChange">转</i>
+                                    <div :style="lineStyle(item.changeLine)">
+                                        <strong>{{index+1}}</strong>
+                                    </div>
+                                    <span>{{item.name}}</span>
+                                    <p>{{item.des}}</p>
+                                </div>
+                            </div>                            
+                        </div>
+                        <div>
+                            <form-item label="最少换乘"/>
+                            <div class="step-map">
+                                <div v-for="(item, index) in mastMinChange" :key="index">
+                                    <i v-if="index === 0">起</i>
+                                    <i v-if="index === mastMinChange.length - 1">始</i>
+                                    <i v-if="item.isChange">转</i>
+                                    <div :style="lineStyle(item.changeLine)">
+                                        <strong>{{index+1}}</strong>
+                                    </div>
+                                    <span>{{item.name}}</span>
+                                    <p>{{item.des}}</p>
+                                </div>
+                            </div>                            
                         </div>
                     </div>
                 </Form>
@@ -46,6 +65,7 @@
 import subWay from './line'
 import {Select, Option, Form, FormItem, Button, Message} from 'element-ui'
 import {cloneDeep, intersection} from 'lodash'
+import { debuglog } from 'util';
 console.log(cloneDeep)
 let ctx = null
 let prev = {x: null, y: null}
@@ -62,40 +82,12 @@ export default {
             endVal: '',
             endStation: '',
             endList: [],
-            minStation: []
+            minStation: {},
+            mastMinChange: [],
+            mastMinStation: []
         }
     },
     computed: {
-        line () {
-            const {minStation} = this
-            if (!minStation.length) {
-                return []
-            }
-            const newMinStation = cloneDeep(minStation)
-            console.log(newMinStation)
-            let startStation = newMinStation[0].line
-            let nextStation = newMinStation[1].line
-            let currentLine = intersection(startStation, nextStation)[0]
-            newMinStation[0].start = currentLine
-            for (let index = 0; index < newMinStation.length; index++) {
-                const element = newMinStation[index];
-                let nextElement = newMinStation[index + 1];
-                if (!nextElement) {
-                    nextElement = element
-                }
-                const some = intersection(element.line, nextElement.line)[0]
-                if (some !== currentLine) {
-                    element.des = `${currentLine}号线换成${some}号线`
-                    element.changeLine = [currentLine, some]
-                    element.isChange = true
-                    currentLine = some
-                } else {
-                    element.changeLine = [currentLine]
-                    element.isChange = false
-                }
-            }
-            return newMinStation
-        },
         isplan () {
             const {startVal, endVal, startStation, endStation} = this
             if (!(startStation && endStation)) {
@@ -183,27 +175,25 @@ export default {
             }
             stepList.add(start)
             jisuan(start, 0)
-            console.log(allStepsList)
-            console.timeEnd('最短站数')
+            k = 1
+            let temp = {}
             for (const key in allStepsList) {
                 if (allStepsList.hasOwnProperty(key)) {
                     const element = allStepsList[key];
                     if (element.length === min) {
-                        this.minStation = element
-                        break
+                        temp[k++] = element
                     }
                 }
             }
+            this.minStation = temp
             // this.num = min
         },
         drawPoint (station, color) {
             let _color = color[0]
-            console.log(color)
             const [x, y] = station.position
             ctx.lineWidth = 2
             // ctx.globalCompositeOperation = 'destination-over'
             if (color.length > 1) {
-                console.log('还站', color)
                 const {length} = color
                 ctx.save()
                 ctx.translate(x * 60, y * 50)
@@ -212,7 +202,7 @@ export default {
                 let rangAngle = length == 2 ? Math.PI : Math.PI / 3 * 2
                 for (let i = 0; i < length; i++) {
                     ctx.beginPath()
-                    console.log(color[i])
+                    ctx.lineWidth = 4
                     ctx.strokeStyle = color[i]
                     ctx.arc(0, 0, 12, startAngle * i, rangAngle * (i + 1), false)
                     ctx.stroke()
@@ -244,7 +234,6 @@ export default {
             
         },
         drawLine (start, end, color = 'rgb(159, 159, 255)') {
-            console.log(color)
             ctx.strokeStyle = color
             ctx.beginPath()
             ctx.lineWidth = 4
@@ -252,9 +241,76 @@ export default {
             ctx.lineTo(end.x * 60, end.y * 50)
             ctx.closePath()
             ctx.stroke()
+        },
+        disposeData (value) {
+            const copyValue = {}
+            for (const key in value) {
+                let newMinStation = copyValue[key] = cloneDeep(value[key])
+                let startStation = newMinStation[0].line
+                let nextStation = newMinStation[1].line
+                let currentLine = intersection(startStation, nextStation)[0]
+                newMinStation[0].start = currentLine
+                for (let index = 0; index < newMinStation.length; index++) {
+                    const element = newMinStation[index];
+                    let nextElement = newMinStation[index + 1];
+                    if (!nextElement) {
+                        nextElement = element
+                    }
+                    const some = intersection(element.line, nextElement.line)[0]
+                    if (some !== currentLine) {
+                        element.des = `${currentLine}号线换成${some}号线`
+                        element.changeLine = [currentLine, some]
+                        element.isChange = true
+                        currentLine = some
+                    } else {
+                        element.changeLine = [currentLine]
+                        element.isChange = false
+                    }
+                }
+            }
+            return copyValue
         }
     },
     watch: {
+        minStation (value) {
+            const copyValue = this.disposeData(value)
+            const {length} = Object.keys(copyValue)
+            if (length === 1) {
+                this.mastMinChange = this.mastMinStation = Object.freeze(copyValue[1])
+                return false
+            }
+            this.mastMinStation = copyValue[1]
+            let minC = 9999
+            console.log(length)
+            for (const key in copyValue) {
+                const scheme = copyValue[key]
+                let c = 0
+                const {length} = scheme
+                for (let i = 0; i < length; i++) {
+                    if (scheme[i].isChange) {
+                        c++
+                    }
+                }
+                if (c < minC) {
+                    minC = c
+                }
+            }
+            console.log(minC)
+            for (const key in copyValue) {
+                const scheme = copyValue[key]
+                let c = 0
+                const {length} = scheme
+                for (let i = 0; i < length; i++) {
+                    if (scheme[i].isChange) {
+                        c++
+                    }
+                }
+                if (c === minC) {
+                    this.mastMinChange = Object.freeze(scheme)
+                    break
+                }
+            }
+        },
         startVal (e) {
             const {sub} = this
             this.startStation = ''
@@ -266,6 +322,9 @@ export default {
             this.endList = Object.freeze(sub[e].list)
         }
     },
+    created () {
+        console.time('map')
+    },
     mounted () {
         this.$nextTick(() => {
             const canvas = this.$refs.map
@@ -276,7 +335,6 @@ export default {
             Object.keys(subWay).forEach(v => {
                 const {lineColor, list, type} = subWay[v]
                 const _list = Object.keys(list)
-                console.log(type)
                 if (type === 0) {
                     const last = list[_list[_list.length - 1]].position
                     console.log(last)
@@ -309,6 +367,7 @@ export default {
                     this.drawPoint(station, _color)
                 })
             })
+            console.timeEnd('map')
         })
     }
 }
@@ -319,6 +378,12 @@ export default {
         display: flex;
         .right {
             flex: 1;
+        }
+    }
+    .map-show {
+        display: flex;
+        &>div {
+            width: 50%;
         }
     }
     .step-map {
