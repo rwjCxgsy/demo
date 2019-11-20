ac7f7b7e0d552c45be9dde0f68142e9d31d3d4f2
renweijun <847679857@qq.com>
2019-11-20 18:13:24 +0800

    d

--- src/views/cdsub.vue
+++ src/views/cdsub.vue
@@ -1,287 +1,293 @@
 <template>
-    <div class="cd-sub">
-        <canvas id="canvas" ref="map"></canvas>
-    </div> 
+  <div class="cd-sub">
+    <canvas id="canvas1" ref="map1"></canvas>
+    <canvas id="canvas2" ref="map2"></canvas>
+  </div>
 </template>
 
 <script>
-import subWay from './line'
-import logoURL from './logo'
-import {start} from './subway'
+import subWay from "./line";
+import logoURL from "./logo";
+import { start } from "./subway";
 // import {Select, Option, Form, FormItem, Button, Message, Image} from 'element-ui'
-import {cloneDeep, intersection} from 'lodash'
-const logo = new Image()
-logo.src = logoURL
-console.log(logo)
+import { cloneDeep, intersection } from "lodash";
+const logo = new Image();
+logo.src = logoURL;
+console.log(logo);
 // let ctx = null
 // let prev = {x: null, y: null}
 export default {
-    name: 'cd-subWay',
-    // components: {Select, Option, Form, FormItem, Button},
-    data () {
-        return {
-            sub: Object.freeze(subWay),
-            startLine: '',
-            startStation: '',
-            startList: [],
-            endLine: '',
-            endStation: '',
-            endList: [],
-            minStation: {},
-            mastMinChange: [],
-            mastMinStation: []
-        }
-    },
-    computed: {
-        isplan () {
-            const {startStation, endStation} = this
-            if (!(startStation && endStation)) {
-                return true
-            }
-            if (startStation === endStation) {
-                return true
-            }
-            return false
-        }
-    },
-    methods: {
-        lineStyle (line) {
-            const {length} = line
-            const findColor = (line) => {
-                return subWay['line' + line].color
-            }
-            const style = {
-                borderTop: '10px solid ' + findColor(line[0]),
-                borderBottom: '10px solid ' + findColor(line[length >= 2 ? 1 : 0]),
-            }
-            return style
-        },
-        planning () {
-            console.time('最短站数')
-            const {startStation, endStation, startList, endList} = this
-            let start = startList[startStation]
-            let end = endList[endStation]
-            let min = 99999
-            const stepList = new Set()
-            let k = 1
-            const allStepsList = {}
-            const find = (station) => {
-                let result = null
-                for (const key in subWay) {
-                    if (subWay.hasOwnProperty(key)) {
-                        const element = subWay[key].list;
-                        const _result = element[station]
-                        if (_result) {
-                            result = _result
-                            break
-                        }
-                    }
-                }
-                if (!result) {
-                    console.log(station)
-                }
-                return result
-            }
-            function jisuan (station, step) {
-                let next = null
-                if (station.name === end.name) {
-                    if (stepList.size < min) {
-                        min = stepList.size
-                    }
-                    allStepsList[++k] = [...stepList]
-                    return false
-                }
-                let {length} = station.next
-                for (let i = 0; i < length; i++) {
-                    let n = station.next[i]
-                    next = find(n)
-                    if (!stepList.has(next)) {
-                        stepList.add(next)
-                        jisuan(next, step + 1)
-                        stepList.delete(next)
-                    }
-                }
-            }
-            stepList.add(start)
-            jisuan(start, 0)
-            k = 1
-            let temp = {}
-            for (const key in allStepsList) {
-                if (allStepsList.hasOwnProperty(key)) {
-                    const element = allStepsList[key];
-                    if (element.length <= min + 2) {
-                        temp[k++] = element
-                    }
-                }
-            }
-            console.log(temp)
-            this.minStation = temp
-            // this.num = min
-        },
-        disposeData (value) {
-            const copyValue = {}
-            for (const key in value) {
-                let newMinStation = copyValue[key] = cloneDeep(value[key])
-                let startStation = newMinStation[0].line
-                let nextStation = newMinStation[1].line
-                let currentLine = intersection(startStation, nextStation)[0]
-                newMinStation[0].start = currentLine
-                for (let index = 0; index < newMinStation.length; index++) {
-                    const element = newMinStation[index];
-                    let nextElement = newMinStation[index + 1];
-                    if (!nextElement) {
-                        nextElement = element
-                    }
-                    const some = intersection(element.line, nextElement.line)[0]
-                    if (some !== currentLine) {
-                        element.des = `${currentLine}号线换成${some}号线`
-                        element.changeLine = [currentLine, some]
-                        element.isChange = true
-                        currentLine = some
-                    } else {
-                        element.changeLine = [currentLine]
-                        element.isChange = false
-                    }
-                }
-            }
-            return copyValue
-        }
-    },
-    watch: {
-        minStation (value) {
-            console.log(value)
-            const copyValue = this.disposeData(value)
-            const {length} = Object.keys(copyValue)
-            if (length === 1) {
-                this.mastMinChange = this.mastMinStation = Object.freeze(copyValue[1])
-                return false
-            }
-            let min = 9999
-            let minTemp = []
-            for (const key in copyValue) {
-                if (copyValue.hasOwnProperty(key)) {
-                    const element = copyValue[key];
-                    if (element.length < min) {
-                        minTemp = element
-                        min = element.length
-                    }
-                }
-            }
-            this.mastMinStation = minTemp
-            let minC = 9999
-            let temp = []
-            for (const key in copyValue) {
-                const scheme = copyValue[key]
-                let c = 0
-                const {length} = scheme
-                for (let i = 0; i < length; i++) {
-                    if (scheme[i].isChange) {
-                        c++
-                    }
-                }
-                if (c < minC) {
-                    temp = scheme
-                }
-            }
-            this.mastMinChange = Object.freeze(temp)
-            console.timeEnd('最短站数')
-        },
-        startLine (e) {
-            this.startStation = ''
-            this.startList = Object.freeze(subWay[e].list)
-        },
-        endLine (e) {
-            this.endStation = ''
-            this.endList = Object.freeze(subWay[e].list)
-        }
-    },
-    mounted () {
-        this.$nextTick(() => {
-            const canvas = this.$refs.map
-            start(canvas)
-        })
+  name: "cd-subWay",
+  // components: {Select, Option, Form, FormItem, Button},
+  data() {
+    return {
+      sub: Object.freeze(subWay),
+      startLine: "",
+      startStation: "",
+      startList: [],
+      endLine: "",
+      endStation: "",
+      endList: [],
+      minStation: {},
+      mastMinChange: [],
+      mastMinStation: []
+    };
+  },
+  computed: {
+    isplan() {
+      const { startStation, endStation } = this;
+      if (!(startStation && endStation)) {
+        return true;
+      }
+      if (startStation === endStation) {
+        return true;
+      }
+      return false;
     }
-}
+  },
+  methods: {
+    lineStyle(line) {
+      const { length } = line;
+      const findColor = line => {
+        return subWay["line" + line].color;
+      };
+      const style = {
+        borderTop: "10px solid " + findColor(line[0]),
+        borderBottom: "10px solid " + findColor(line[length >= 2 ? 1 : 0])
+      };
+      return style;
+    },
+    planning() {
+      console.time("最短站数");
+      const { startStation, endStation, startList, endList } = this;
+      let start = startList[startStation];
+      let end = endList[endStation];
+      let min = 99999;
+      const stepList = new Set();
+      let k = 1;
+      const allStepsList = {};
+      const find = station => {
+        let result = null;
+        for (const key in subWay) {
+          if (subWay.hasOwnProperty(key)) {
+            const element = subWay[key].list;
+            const _result = element[station];
+            if (_result) {
+              result = _result;
+              break;
+            }
+          }
+        }
+        if (!result) {
+          console.log(station);
+        }
+        return result;
+      };
+      function jisuan(station, step) {
+        let next = null;
+        if (station.name === end.name) {
+          if (stepList.size < min) {
+            min = stepList.size;
+          }
+          allStepsList[++k] = [...stepList];
+          return false;
+        }
+        let { length } = station.next;
+        for (let i = 0; i < length; i++) {
+          let n = station.next[i];
+          next = find(n);
+          if (!stepList.has(next)) {
+            stepList.add(next);
+            jisuan(next, step + 1);
+            stepList.delete(next);
+          }
+        }
+      }
+      stepList.add(start);
+      jisuan(start, 0);
+      k = 1;
+      let temp = {};
+      for (const key in allStepsList) {
+        if (allStepsList.hasOwnProperty(key)) {
+          const element = allStepsList[key];
+          if (element.length <= min + 2) {
+            temp[k++] = element;
+          }
+        }
+      }
+      console.log(temp);
+      this.minStation = temp;
+      // this.num = min
+    },
+    disposeData(value) {
+      const copyValue = {};
+      for (const key in value) {
+        let newMinStation = (copyValue[key] = cloneDeep(value[key]));
+        let startStation = newMinStation[0].line;
+        let nextStation = newMinStation[1].line;
+        let currentLine = intersection(startStation, nextStation)[0];
+        newMinStation[0].start = currentLine;
+        for (let index = 0; index < newMinStation.length; index++) {
+          const element = newMinStation[index];
+          let nextElement = newMinStation[index + 1];
+          if (!nextElement) {
+            nextElement = element;
+          }
+          const some = intersection(element.line, nextElement.line)[0];
+          if (some !== currentLine) {
+            element.des = `${currentLine}号线换成${some}号线`;
+            element.changeLine = [currentLine, some];
+            element.isChange = true;
+            currentLine = some;
+          } else {
+            element.changeLine = [currentLine];
+            element.isChange = false;
+          }
+        }
+      }
+      return copyValue;
+    }
+  },
+  watch: {
+    minStation(value) {
+      console.log(value);
+      const copyValue = this.disposeData(value);
+      const { length } = Object.keys(copyValue);
+      if (length === 1) {
+        this.mastMinChange = this.mastMinStation = Object.freeze(copyValue[1]);
+        return false;
+      }
+      let min = 9999;
+      let minTemp = [];
+      for (const key in copyValue) {
+        if (copyValue.hasOwnProperty(key)) {
+          const element = copyValue[key];
+          if (element.length < min) {
+            minTemp = element;
+            min = element.length;
+          }
+        }
+      }
+      this.mastMinStation = minTemp;
+      let minC = 9999;
+      let temp = [];
+      for (const key in copyValue) {
+        const scheme = copyValue[key];
+        let c = 0;
+        const { length } = scheme;
+        for (let i = 0; i < length; i++) {
+          if (scheme[i].isChange) {
+            c++;
+          }
+        }
+        if (c < minC) {
+          temp = scheme;
+        }
+      }
+      this.mastMinChange = Object.freeze(temp);
+      console.timeEnd("最短站数");
+    },
+    startLine(e) {
+      this.startStation = "";
+      this.startList = Object.freeze(subWay[e].list);
+    },
+    endLine(e) {
+      this.endStation = "";
+      this.endList = Object.freeze(subWay[e].list);
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      const canvas1 = this.$refs.map1;
+      const canvas2 = this.$refs.map2;
+      start(canvas1, canvas2);
+    });
+  }
+};
 </script>
 
 <style lang="less">
-    .station-des {
-        position: fixed;
-        background-color: #fff;
-        border-radius: 4px;
-        padding: 10px;
-        box-shadow: 0 1px 6px 0 rgba(32,33,36,0.28);
-        strong {
-            font-size: 12px;
-        }
-        span {
-            font-size: 12px;
-        }
+canvas {
+    position: absolute;
+    left: 0;
+    top: 0;
+}
+.station-des {
+  position: fixed;
+  background-color: #fff;
+  border-radius: 4px;
+  padding: 10px;
+  box-shadow: 0 1px 6px 0 rgba(32, 33, 36, 0.28);
+  strong {
+    font-size: 12px;
+  }
+  span {
+    font-size: 12px;
+  }
+}
+.cd-sub {
+  height: 100vh;
+  background-color: rgb(248, 248, 248);
+  display: flex;
+  align-items: center;
+  .right {
+    flex: 1;
+  }
+}
+.map-show {
+  display: flex;
+  & > div {
+    width: 50%;
+  }
+}
+.step-map {
+  height: 450px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  justify-content: flex-start;
+  align-content: flex-start;
+  & > div {
+    height: 45px;
+    margin-right: 20px;
+    display: flex;
+    align-items: center;
+    position: relative;
+    i {
+      position: absolute;
+      left: 0;
+      top: 50%;
+      transform: translate(0, -50%);
+      font-style: normal;
+      border-radius: 50%;
+      font-size: 14px;
     }
-    .cd-sub {
-        height: 100vh;
-        background-color: rgb(248, 248, 248);
-        display: flex;
-        align-items: center;
-        .right {
-            flex: 1;
-        }
+    div {
+      display: inline-block;
+      margin-left: 20px;
+      width: 20px;
+      height: 0;
+      border-radius: 50%;
+      position: relative;
+      strong {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        color: #fff;
+        font-size: 14px;
+      }
     }
-    .map-show {
-        display: flex;
-        &>div {
-            width: 50%;
-        }
+    span {
+      display: inline-block;
+      min-width: 100px;
+      margin-left: 10px;
     }
-    .step-map {
-        height: 450px;
-        display: flex;
-        flex-direction: column;
-        flex-wrap: wrap;
-        align-items: flex-start;
-        justify-content: flex-start;
-        align-content: flex-start;
-        &>div {
-            height: 45px;
-            margin-right: 20px;
-            display: flex;
-            align-items: center;
-            position: relative;
-            i {
-                position: absolute;
-                left: 0;
-                top: 50%;
-                transform: translate(0, -50%);
-                font-style: normal;
-                border-radius: 50%;
-                font-size: 14px;
-            }
-            div {
-                display: inline-block;
-                margin-left: 20px;
-                width: 20px;
-                height: 0;
-                border-radius: 50%;
-                position: relative;
-                strong {
-                    position: absolute;
-                    left: 50%;
-                    top: 50%;
-                    transform: translate(-50%, -50%);
-                    color: #fff;
-                    font-size: 14px;
-                }
-            }
-            span {
-                display: inline-block;
-                min-width: 100px;
-                margin-left: 10px;
-            }
-            p {
-                color: red;
-                font-size: 12px;
-            }    
-        }
-
+    p {
+      color: red;
+      font-size: 12px;
     }
+  }
+}
 </style>
--- src/views/subway.js
+++ src/views/subway.js
@@ -2,6 +2,8 @@ import { cloneDeep, intersection } from 'lodash'
 import subWay from './line'
 import logoURL from './logo'
 const logo = new Image();
+const GX = 60
+const GY = 50
 !(async () => {
     function load () {
         return new Promise(r => {
@@ -14,7 +16,8 @@ const logo = new Image();
     await load()
 })()
 
-let ctx = null
+let lineCTX = null
+let stationCTX = null
 const offset = {
     x: 0,
     y: 0
@@ -23,6 +26,7 @@ let prev = { x: null, y: null }
 
 const lineList = []
 const stationList = []
+let drawes = []
 
 const findStation = (station, line) => {
     for (const item of stationList) {
@@ -54,59 +58,63 @@ class SubwayStation {
         this.isActive = false
         this.isSelected = false
         this.descriptionContainer = null
-        this.isDraw = false
+        // this.isDraw = false
         stationList.push(this)
     }
     draw() {
         const { name, x, y, textAlign, textBaseline, direction, offset, radius, line, isDraw = false} = this
-        if (isDraw) {return}
-        this.isDraw = true
+        // if (isDraw) {return}
+        // this.isDraw = true
+        if (drawes.includes(this)) {
+            return
+        }
+        drawes.push(this)
         const color = line[0].color
         const { length } = line
-        ctx.beginPath()
-        ctx.arc(x * 60, y * 50, radius, 0, Math.PI * 2, false)
-        ctx.closePath()
-        ctx.fillStyle = color
-        ctx.fill()
-        // if (length > 1) {
-        //     ctx.fillStyle = '#fff'
-        //     ctx.beginPath()
-        //     ctx.arc(x * 60, y * 50, radius * 2, 0, Math.PI * 2, false)
-        //     ctx.fill()
-        //     ctx.drawImage(logo, x * 60 - 10, y * 50 - 10, 20, 20)
-        //     ctx.save()
-        //     ctx.translate(x * 60, y * 50)
-        //     ctx.rotate((Math.PI * 2) / 8 * 5)
-        //     let startAngle = length == 3 ? (Math.PI * 2) / 3 : Math.PI
-        //     let rangAngle = length == 2 ? Math.PI : Math.PI / 3 * 2
-        //     for (let i = 0; i < length; i++) {
-        //         ctx.beginPath()
-        //         ctx.lineCap = "round"
-        //         ctx.lineWidth = radius / 2
-        //         ctx.strokeStyle = color[i]
-        //         ctx.arc(0, 0, radius * 2, startAngle * i, rangAngle * (i + 1), false)
-        //         ctx.stroke()
-        //     }
-        //     ctx.restore()
-        // } else {
-        //     ctx.beginPath()
-        //     ctx.arc(x * 60, y * 50, radius, 0, Math.PI * 2, false)
-        //     ctx.closePath()
-        //     ctx.fillStyle = color
-        //     ctx.fill()
-        // }
-        ctx.fillStyle = color
-        ctx.font = '12px sans-serif'
-        ctx.textAlign = textAlign
-        ctx.textBaseline = textBaseline
-        ctx.direction = direction
-        ctx.fillText(name, x * 60 + offset[0] + (textAlign === 'end' ? (-16) : (16)), y * 50 + offset[1])
+        if (length > 1) {
+            stationCTX.fillStyle = '#fff'
+            stationCTX.beginPath()
+            stationCTX.arc(x * GX, y * GY, radius * 2, 0, Math.PI * 2, false)
+            stationCTX.fill()
+            stationCTX.drawImage(logo, x * GX - 10, y * GY - 10, 20, 20)
+            stationCTX.save()
+            stationCTX.translate(x * GX, y * GY)
+            stationCTX.rotate((Math.PI * 2) / 8 * 5)
+            let startAngle = length == 3 ? (Math.PI * 2) / 3 : Math.PI
+            let rangAngle = length == 2 ? Math.PI : Math.PI / 3 * 2
+            for (let i = 0; i < length; i++) {
+                stationCTX.beginPath()
+                stationCTX.lineCap = "round"
+                stationCTX.lineWidth = radius / 2
+                stationCTX.strokeStyle = color[i]
+                stationCTX.arc(0, 0, radius * 2, startAngle * i, rangAngle * (i + 1), false)
+                stationCTX.stroke()
+            }
+            stationCTX.restore()
+        } else {
+            stationCTX.beginPath()
+            stationCTX.arc(x * GX, y * GY, radius, 0, Math.PI * 2, false)
+            stationCTX.closePath()
+            stationCTX.fillStyle = color
+            stationCTX.fill()
+
+            stationCTX.arc(x * GX, y * GY, radius, 0, Math.PI * 2, false)
+            stationCTX.lineWidth = '2'
+            stationCTX.strokeStyle = '#fff'
+            stationCTX.stroke()
+        }
+        stationCTX.font = '12px sans-serif'
+        stationCTX.textAlign = textAlign
+        stationCTX.textBaseline = textBaseline
+        stationCTX.direction = direction
+        stationCTX.fillText(name, x * GX + offset[0] + (textAlign === 'end' ? (-16) : (16)), y * GY + offset[1])
+        stationCTX.restore()
     }
     updata() {
         const { x, y, radius } = this
         const offsetY = offset.y
         const offsetX = offset.x
-        const isMoveUp = ((x * 60 - offsetX) ** 2 + (y * 50 - offsetY) ** 2) <= ((radius + 2) ** 2)
+        const isMoveUp = ((x * GX - offsetX) ** 2 + (y * GY - offsetY) ** 2) <= ((radius + 2) ** 2)
         if (isMoveUp) {
             if (this.radius < 8) {
                 this.radius += 1
@@ -129,7 +137,7 @@ class SubwayStation {
         const { x, y, name, line, next, color } = this
         let div = this.descriptionContainer = document.createElement('div')
         div.classList.add('station-des')
-        div.style.cssText = `position: fixed; left: ${60 * x + 25}px; top: ${y * 50 + 16}px`
+        div.style.cssText = `position: fixed; left: ${GX * x + 25}px; top: ${y * GY + 16}px`
         const nextStation = () => {
             let result = ''
             for (const iterator of next) {
@@ -177,6 +185,7 @@ class Line {
         this.descriptionContainer = null
         this.stations = []
         this.prevDirection = {}
+        this.active = false
         for (const station of list) {
             const r = findStation(station, this)
             if (r) {
@@ -187,43 +196,32 @@ class Line {
         }
     }
     draw() {
-        const {name, width, stations: list } = this
+        const {name, width, stations: list, active } = this
         // const direction = ''
         const { length } = list
-        const [ startPosition ] = list
-        let {x: x1, y: y1} = startPosition
-        const color = startPosition.color
-        ctx.strokeStyle = color
-        ctx.lineWidth = width
-        list[0].draw()
-        for (let i = 1; i < length; i++) {
-            ctx.beginPath()
-            const {x: x2, y: y2} = list[i]
-            const arc = this.getK(x1, y1, x2, y2)
-            ctx.moveTo(x1 * 60 + Math.sin(arc) * 8, y1 * 50 + Math.cos(arc) * 8)
-            ctx.lineTo(x2 * 60 - Math.sin(arc) * 8, y2 * 50 - Math.cos(arc) * 8)
+        let [ ins ] = list
+        let {x: x1, y: y1} = ins
+        const color = ins.color
+        lineCTX.strokeStyle = color
+        lineCTX.lineWidth = width
+        lineCTX.lineCap = "round"
+        ins.draw()
+        for (let i = 0; i < length; i++) {
+            const {x: x2, y: y2} = ins = list[i]
+            lineCTX.beginPath()
+            lineCTX.moveTo(x1 * GX, y1 * GY)
+            lineCTX.lineTo(x2 * GX, y2 * GY)
             x1 = x2
             y1 = y2
-            // if (name === 'line7' && i === length - 1) {
-            //     ctx.closePath()
-            // }
-            ctx.closePath()
-            ctx.stroke()
-            list[i].draw()
-        }
-    }
-    getK (x1, y1, x2, y2) {
-        if (x1 === x2) {
-            return y2 > y1 ? 0 : Math.PI
-        }
-        if (y1 === y2) {
-            return x2 < x1 ? (- Math.PI / 2) : Math.PI / 2
-        }
-        if (y2 < y1) {
-            console.log(x1, y1, x2, y2)
-            return Math.atan((x2 - x1) / (y2 - y1))
-        } else {
-            return Math.atan((y2 - y1) / (x2 - x1))
+            if (name === 'line7' && i === length - 1) {
+                x2 = list[0].x
+                y2 = list[0].y
+                lineCTX.moveTo(x1 * GX, y1 * GY)
+                lineCTX.lineTo(x2 * GX, y2 * GY)
+            }
+            lineCTX.stroke()  
+            // ctx.closePath()
+            ins.updata()
         }
     }
     quadraticCurvePoint (x1, y1, x2, y2, ctx) {
@@ -316,15 +314,15 @@ class Line {
         const SY = start.y
         const EX = end.x
         const EY = end.y
-        const maxX = Math.max(SX, EX) * 60
-        const minX = Math.min(SX, EX) * 60
-        const maxY = Math.max(SY, EY) * 50
-        const minY = Math.min(SY, EY) * 50
+        const maxX = Math.max(SX, EX) * GX
+        const minX = Math.min(SX, EX) * GX
+        const maxY = Math.max(SY, EY) * GY
+        const minY = Math.min(SY, EY) * GY
         if (x - 4 > maxX || x + 4 < minX || y - 4 > maxY || y + 4 < minY) {
             return false
         }
-        const k = ((EY - SY) * 50) / ((EX - SX) * 60)
-        let b = SY * 50 - SX * 60 * k
+        const k = ((EY - SY) * GY) / ((EX - SX) * GX)
+        let b = SY * GY - SX * GX * k
         if (!isFinite(k)) {
             return Math.abs(x - maxX) <= 4
         }
@@ -335,27 +333,32 @@ class Line {
     }
 }
 
-function launch(canvas) {
+function launch(line, station) {
     const canvasWidth = 1200
     const canvasHeight = 900
-    canvas.addEventListener('mousemove', mousemove)
+    station.addEventListener('mousemove', mousemove)
     function mousemove(e) {
         const { offsetX, offsetY } = e
         offset.x = offsetX
         offset.y = offsetY
     }
-    canvas.width = canvasWidth
-    canvas.height = canvasHeight
-    ctx = canvas.getContext('2d')
+    line.width = canvasWidth
+    line.height = canvasHeight
+    station.width = canvasWidth
+    station.height = canvasHeight
+
+    lineCTX = line.getContext('2d')
+    stationCTX = station.getContext('2d')
 
     for (const k in subWay) {
         lineList.push(new Line(subWay[k]))
     }
     console.log(lineList, stationList)
-    let isDraw = []
     function animate() {
-        // requestAnimationFrame(animate)
-        ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+        requestAnimationFrame(animate)
+        lineCTX.clearRect(0, 0, canvasWidth, canvasHeight)
+        stationCTX.clearRect(0, 0, canvasWidth, canvasHeight)
+        drawes = []
         for (let line of lineList) {
             line.updata()
         }
@@ -371,6 +374,6 @@ function launch(canvas) {
 }
 
 
-export function start(canvas) {
-    launch(canvas)
+export function start(line, station) {
+    launch(line, station)
 }
