3016318ea025e6e80065dfa2cb509be6b670d18e
renweijun <847679857@qq.com>
2019-11-26 16:17:02 +0800

    d

---
+++ src/views/fall.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="fall">
+      <canvas id="canvas" ref="canvas"></canvas>
+  </div>
+</template>
+
+<script>
+
+class Boll {
+    constructor (x, y, ctx, root) {
+        this.x = x
+        this.y = y
+        this.color = this.randomColor()
+        this.ctx = ctx
+        this.radius = Math.floor(Math.random() * 20) + 10
+        this.g = 2,
+        this.vy = 0
+        this.speed = this.radius / 1000000000
+        this.k = 0
+        this.destory = false
+        this.root = root
+        this.maxHieght = y
+    }
+    draw () {
+        const {ctx, x, y, color, radius} = this
+        ctx.fillStyle = color
+        ctx.beginPath()
+        ctx.arc(x, y, radius, 0, Math.PI * 2, false)
+        ctx.closePath()
+        ctx.fill()
+    }
+    randomColor () {
+        const r = Math.random().toString(16).substr(2, 6)
+        return '#' + r
+    }
+    updata () {
+        const {g, initSpeed, k, y, speed, root, radius, vy} = this
+        const {height} = root
+        this.y += vy;
+        this.vy += g;
+        if (Math.abs(this.vy) - 2 <= 0) {
+            this.maxHieght = this.y
+        }
+        if( this.y >= height - 100 - radius ){
+            this.y = height - radius - 100;
+            this.vy = - vy * 0.75;
+        }
+        if (height - 100 - this.maxHieght - radius <= 10) {
+            this.y = height - radius - 100;
+            this.destory = true
+        }
+        this.draw()
+        // this.k++
+    }
+}
+
+class Fall {
+    constructor (canvas) {
+        this.canvas = canvas
+        this.ctx = canvas.getContext('2d')
+        this.child = new Set()
+        this.init()
+        this.animate()
+    }
+    init () {
+        const {canvas, ctx} = this
+        canvas.width = this.width = document.documentElement.clientWidth
+        canvas.height = this.height = document.documentElement.clientHeight
+        canvas.addEventListener('click', e => {
+            const {clientX, clientY} = e
+            if (clientY > this.height - 200) {
+                return
+            }
+            this.child.add(new Boll(clientX, clientY, ctx, this))
+        })
+    }
+    animate () {
+        const {width, height, color, ctx, child} = this
+        requestAnimationFrame(this.animate.bind(this))
+        ctx.clearRect(0, 0, width, height)
+        ctx.beginPath()
+        ctx.moveTo(0, height - 100)
+        ctx.lineTo(width, height - 100)
+        ctx.strokeStyle = '#999'
+        ctx.stroke()
+        ctx.closePath()
+        for (const ball of child) {
+            if (ball.destory) {
+                child.delete(ball)
+            } else {
+                ball.updata()
+            }
+        }
+        console.log(child.size)
+    }
+}
+
+
+export default {
+    name: 'fall',
+    mounted () {
+        new Fall(this.$refs.canvas)
+    }
+}
+</script>
+
+<style lang="less">
+    .fall {
+        height: 100vh;
+    }
+</style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="home">
         <router-link to="/cdsub">成都地图</router-link>
-        <p>{{num}}</p>
         <router-link to="/range">日期选择器</router-link>
+        <router-link to="/scroll">scroll</router-link>
+        <router-link to="/fall">fall</router-link>
     </div>
 </template>
 
@@ -29,33 +30,9 @@ export default {
 }
 </script>
 
-<style scoped lang="less">
-    div {
-        position: relative;
-        // div {
-        //     height: 100px;
-        //     color: #fff;
-        // }
-    }
-    .left {
-        float: left;
-        width: 100px;
-        background-color: blue;
-    }
-    .right {
-        left: 100px;
-        right: 0;
-        position: absolute;
-        // margin-left: 100px;
-        background-color: yellow;
-    }
-    button {
-        width: 20cm;
-    }
-    .home::after {
-        content: '::'
-    }
-    .home:after{
-        content: ':'
+<style lang="less">
+    a {
+        display: block;
+        height: 30px;
     }
 </style>
