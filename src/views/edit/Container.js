6561d0e60c6e331cdaf180922f5186405eb02bd4
renweijun <847679857@qq.com>
2020-01-10 17:55:11 +0800

    逻辑结构

--- src/views/edit/Container.js
+++ src/views/edit/Container.js
@@ -1,29 +1,110 @@
 import Store from "./Store";
-import ImageContainer from "./Image";
-import Observe from "./Observe";
-import Text from './Text'
 
-class Container {
-    constructor () {
-        let {zoomIndex} = Store
-        let id = zoomIndex++
+export class Container {
+    constructor (x = 0, y = 0, width = 0, height = 0, name = '') {
+        let id = Store.zoomIndex++
         this.id = `id-${id}`
         this.zIndex = id
         this.show = true
+        this.focus = false
+        this.width = width
+        this.height = height
+        this.x = x
+        this.y = y
+        this.name = name
+        this.color = '#' + Math.random().toString(16).slice(2,8)
+        this.transform = {}
+        this.lineDashOffset = 0
     }
-}
 
-export default Container
-
-export function createContainer ({type, date}) {
-    let container = null
-    if (type === 'text') {
-        const {input} = Store
-        input.focus()
-        container =  new Text()
-    } else {
-        container =  new ImageContainer(date)
+    renderWrap () {
+        let {x, y, width, height, color, focus, lineDashOffset} = this
+        if (!focus) {
+            return
+        }
+        const {ctx} = Store
+        ctx.save()
+        if (lineDashOffset > 50) {
+            this.lineDashOffset = -1
+        }
+        ctx.lineDashOffset = this.lineDashOffset++
+        x -= 1.5
+        width += 3
+        width += 1
+        y -= 1.5
+        height += 3
+        ctx.strokeStyle = color
+        ctx.lineWidth = 1
+        ctx.setLineDash([10, 5]);
+        ctx.strokeRect(x, y, width, height);
+        ctx.restore()
     }
-    Store.addContent(container)
-    Observe.emit('zoom', Store.includes)
+    isFocus () {
+        const {mouse} = Store
+        const {layerX = 0, layerY = 0} = mouse
+        const {x, y, width, height} = this
+        if (layerX >= x && layerX <= (x + width) && layerY >= y && layerY <= (y + height)) {
+            return this
+        }
+        return false
+    }
+
+    move () {
+        const {mouseDown, mouse, focus, lastMouse} = Store
+        if (mouseDown && focus === this) {
+            const {x, y} = focus
+            let offsetX = 0
+            let offsetY = 0
+            const {layerX, layerY} = mouse
+            if (!Store.lastMouse) {
+                Store.lastMouse = {
+                    x: layerX,
+                    y: layerY
+                }
+            } else {
+                const {x: lx, y: ly} = lastMouse
+                offsetX = layerX - lx
+                offsetY = layerY - ly
+                Store.lastMouse = {
+                    x: layerX,
+                    y: layerY
+                }
+            }
+            this.x = x + offsetX
+            this.y = y + offsetY
+        }
+    }
+
+    update () {
+        const {ctx, mouseDown} = Store
+        let set = Store.riseContent(false)
+        let maxZoom = false
+        for (let item of set) {
+            const _focus = item.isFocus()
+            if (_focus) {
+                if (!maxZoom) {
+                    Store.focus = item
+                    item.focus = true
+                    maxZoom = true
+                } else {
+                    item.focus = false
+                }
+            } else {
+                item.focus = false
+            }
+        }
+        if (maxZoom) {
+            ctx.canvas.style.cursor = 'move'
+            this.move()
+        } else {
+            Store.focus = null
+            ctx.canvas.style.cursor = ''
+        }
+
+        if (!(mouseDown && Store.focus)) {
+            this.isFocus()
+        }
+        this.renderWrap()
+        this.render()
+    }
 }
--- src/views/edit/Store.js
+++ src/views/edit/Store.js
@@ -2,10 +2,22 @@ import Observer from "./Observe";
 
 class Store {
     constructor() {
+        this.focus = false
+        this.mouseDown = false
+        this.ctx = null
+        this.input = null
+        this.mouse = {}
+        this.zoomIndex = 1
+        this._canvasMargin = 50
+        this.init()
+        this.lastMouse = null
+    }
+
+    init () {
         Observer.on('change', data => {
             console.log('change')
             const {id, show} = data
-            for (let c of Store.includes) {
+            for (let c of this.includes) {
                 if (c.id === id) {
                     console.log(c)
                     c.show = show
@@ -15,48 +27,80 @@ class Store {
         })
     }
 
-    _canvas = null
-    set ctx (value) {
-        this._canvas = value
-    }
-    get ctx () {
-        return this._canvas
-    }
-
-
-    _input = null
-    set input (value) {
-        this._input = value
-    }
-    get input () {
-        return this._input
-    }
-
-    _mouse = null
-
-    set mouse (event) {
-        this._mouse = event
-    }
-
     forceUpdate () {
         const {ctx} = this
         const {width, height} = ctx.canvas
         ctx.clearRect(0,0, width, height)
-        let aort = Store.riseContent()
-        for (let content of aort) {
+        let sort = this.riseContent()
+        // let set = Store.riseContent(false)
+        // let maxZoom = false
+        // if (this.focus)
+        // for (let content of set) {
+        //     if (content.show) {
+        //         if (content.isFocus()) {
+        //             if (!maxZoom && !this.mouseDown) {
+        //                 content.focus = true
+        //                 this.focus = content
+        //                 maxZoom = true
+        //             } else if (this.mouseDown) {
+        //                 content.focus = true
+        //                 maxZoom = true
+        //             } else {
+        //                 content.focus = false
+        //             }
+        //         } else {
+        //             content.focus = false
+        //         }
+        //     } else {
+        //         content.focus = false
+        //     }
+        // }
+        // if (!maxZoom) {
+        //     this.focus = null
+        //     this.ctx.canvas.style.cursor = ''
+        // } else {
+        //     const {ctx, mouseDown, mouse, focus, lastMouse} = this
+        //     ctx.canvas.style.cursor = 'move'
+        //     if (mouseDown) {
+        //         const {x, y} = focus
+        //         let offsetX = 0
+        //         let offsetY = 0
+        //         const {layerX, layerY} = mouse
+        //         if (!this.lastMouse) {
+        //             this.lastMouse = {
+        //                 x: layerX,
+        //                 y: layerY
+        //             }
+        //         } else {
+        //             const {x: lx, y: ly} = lastMouse
+        //             offsetX = layerX - lx
+        //             offsetY = layerY - ly
+        //             this.lastMouse = {
+        //                 x: layerX,
+        //                 y: layerY
+        //             }
+        //         }
+        //         this.focus.x = x + offsetX
+        //         this.focus.y = y + offsetY
+        //     } else {
+        //         this.lastMouse = null
+        //     }
+        // }
+        for (let content of sort) {
             if (content.show) {
                 content.update()
             }
         }
     }
 
-    get mouse () {
-        return this._mouse
-    }
-
-    static riseContent () {
-        return Store._content.sort((a, b) => {
-            return a.zIndex  > b.zIndex  ? 1 : -1
+    riseContent (rise = true) {
+        let clone = [...Store._content]
+        return clone.sort((a, b) => {
+            if (rise) {
+                return a.zIndex > b.zIndex ? 1 : -1
+            } else {
+                return a.zIndex < b.zIndex ? 1 : -1
+            }
         })
     }
 
@@ -69,16 +113,10 @@ class Store {
     }
 
     addContent (container) {
+        console.log(Store._content)
         Store._content.push(container)
     }
 
-    _zoomIndex = 1
-    get zoomIndex () {
-        return this._zoomIndex
-    }
-    set zoomIndex (value) {
-        this._zoomIndex = value
-    }
 }
 
 export default new Store()
